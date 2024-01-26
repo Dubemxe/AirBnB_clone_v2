3fada570bf16df26bc22d3f37effe5e274c9112f
Chidubem Uchendu <dubemxe@gmail.com>
2024-01-26 01:31:03 -0800

    C <text>

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Defines a script that listens on 0.0.0.0, port 5000 and displays 'Hello HBNB'
+And on another route - /hbnb displays 'HBNB'
+On a new route - /c/<text> displays 'C' followed by the content of '<text>'
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def display():
+    """Dispalys Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    """Dispalys HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_C(text):
+    """Dispalys C followed by the content of <text>"""
+    if text == "_":
+        return " "
+    else:
+        return f"C {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
