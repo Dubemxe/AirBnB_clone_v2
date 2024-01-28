8ad93c783a6564b22e2bfdd0523a8973429d7427
Chidubem Uchendu <dubemxe@gmail.com>
2024-01-28 11:38:59 -0800

    again

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,11 +4,11 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create a User with localhost
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
 
--- Grant all privileges on the hbnb_dev_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
 -- Grant Usage
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost'
 
 -- Grant SELECT privilege on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Grant all privileges on the hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
